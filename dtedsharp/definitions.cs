43f2f5231dacb02a109981c7f5749d3905af9a51
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2023-11-14 11:56:25 -0600

    added acc class

--- dtedsharp/definitions.cs
+++ dtedsharp/definitions.cs
@@ -1,6 +1,11 @@
-int UHL_SIZE = 80;
-int DSI_SIZE = 648;
-int ACC_SIZE = 2700;
+namespace definitions
+{
+    public static class Constants
+    {
+        public const int UHL_SIZE = 80;
+        public const int DSI_SIZE = 648;
+        public const int ACC_SIZE = 2700;
 
-string _UTF8 = "utf-8";
-
+        public const string _UTF8 = "utf-8";
+    }
+}
---
+++ dtedsharp/records/acc.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using definitions;
+
+namespace acc{
+    public class AccuracyDescription
+    {
+        public int? AbsoluteHorizontal { get; private set; }
+        public int? AbsoluteVertical { get; private set; }
+        public int? RelativeHorizontal { get; private set; }
+        public int? RelativeVertical { get; private set; }
+        private readonly byte[] _data;
+        private static readonly byte[] _SENTINEL = "ACC"u8.ToArray();
+
+        private AccuracyDescription(int? absoluteHorizontal, int? absoluteVertical, int? relativeHorizontal, int? relativeVertical, byte[] data)
+        {
+            AbsoluteHorizontal = absoluteHorizontal;
+            AbsoluteVertical = absoluteVertical;
+            RelativeHorizontal = relativeHorizontal;
+            RelativeVertical = relativeVertical;
+            _data = data;
+        }
+
+        public static AccuracyDescription FromBytes(byte[] data)
+        {
+            if (data.Length < Constants.ACC_SIZE)
+            {
+                throw new ArgumentException($"The Accuracy Description record is {Constants.ACC_SIZE} bytes but was provided {data.Length} bytes");
+            }
+
+            using (var bufferedData = new MemoryStream(data))
+            {
+                var sentinel = new byte[3];
+                bufferedData.Read(sentinel, 0, 3);
+                if (!CompareArrays(sentinel, _SENTINEL))
+                {
+                    throw new InvalidFileException($"Accuracy Description Records must start with '{System.Text.Encoding.ASCII.GetString(_SENTINEL)}'. Found: {System.Text.Encoding.ASCII.GetString(sentinel)}");
+                }
+
+                var absHorBytes = new byte[4];
+                var absVertBytes = new byte[4];
+                var relHorBytes = new byte[4];
+                var relVertBytes = new byte[4];
+                bufferedData.Read(absHorBytes, 0, 4);
+                bufferedData.Read(absVertBytes, 0, 4);
+                bufferedData.Read(relHorBytes, 0, 4);
+                bufferedData.Read(relVertBytes, 0, 4);
+
+                return new AccuracyDescription(
+                    TryConvertToInt(absHorBytes),
+                    TryConvertToInt(absVertBytes),
+                    TryConvertToInt(relHorBytes),
+                    TryConvertToInt(relVertBytes),
+                    data
+                );
+            }
+        }
+
+        private static bool CompareArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length) return false;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i]) return false;
+            }
+            return true;
+        }
+
+        private static int? TryConvertToInt(byte[] bytes)
+        {
+            if (bytes.Length < 4) return null;
+            return BitConverter.ToInt32(bytes, 0);
+        }
+    }
+
+    public class InvalidFileException : Exception
+    {
+        public InvalidFileException(string message) : base(message)
+        {
+        }
+    }
+}
