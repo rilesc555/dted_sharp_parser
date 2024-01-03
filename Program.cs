41377ada28e07528629b7e91014893216f0378e2
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2024-01-03 17:39:33 -0600

    Final final

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@ using tile;
 
 Tile sampleTile = new("/Users/riley/Projects/n41_w071_1arc_v3.dt2");
 
-//to find elevation at a point, first create a LatLon object using "new LatLon(latitude, longitude)", where latitude and longitude are doubles
-//then call tileName.getElevation(LatLon)
-//
+/* to find elevation at a point, first create a LatLon object using "new LatLon(latitude, longitude)", where latitude and longitude are doubles
+then call tileName.getElevation(LatLon)
 
+TODO:
+* handle error values
+* add checksum verification
+* add find greatest function
+* add find lowest function
+* add find average function
+* refine getting elevation at a point
+ */
+ 
 Console.WriteLine(sampleTile.getElevation(new LatLon(sampleTile.DSI.NorthWestCorner.Latitude, sampleTile.DSI.NorthWestCorner.Longitude)));
